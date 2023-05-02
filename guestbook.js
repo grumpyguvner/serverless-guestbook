e4b0f75e8b78bd0a0166bc80faabae7cc4a2c041
“Mark <mark@grumpyguvner.com>
2023-05-02 13:22:03 -0400

    Added url endpoints

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://eu-gb.functions.appdomain.cloud/api/v1/web/66d9ac9e-8330-4ce3-a6e1-c56c39af4ebe/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
